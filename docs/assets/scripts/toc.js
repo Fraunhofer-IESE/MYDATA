7af7b6be93bf415d96514cc40e67e1761b90da23
Denis Feth <denis.feth@iese.fraunhofer.de>
2025-12-04 14:38:57 +0100

    Initial setup of GitHub Pages structure

---
+++ docs/assets/scripts/toc.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+  var toc = "<ul id='contentList' class='collapsed'>";
+  $("h2").slice(1).each(function(i, el){
+    var current = $(el);
+    current.attr("id", "heading" + i);
+    toc += "<li><a href='#heading" + i + "'>" + current.text() + "</a></li>";
+  });
+  toc += "</ul>";
+  $("#toc").html(toc);
+});
+
+document.getElementById("toggleButton").addEventListener("click", function() {
+    var contentList = document.getElementById("contentList");
+    if (contentList.classList.contains("collapsed")) {
+        contentList.classList.remove("collapsed");
+        contentList.classList.add("expanded");
+        this.textContent = "Inhaltsverzeichnis ausblenden";
+    } else {
+        contentList.classList.remove("expanded");
+        contentList.classList.add("collapsed");
+        this.textContent = "Inhaltsverzeichnis anzeigen";
+    }
+});
---
+++ docs/index.md
@@ -0,0 +1,8 @@
+# MY DATA Control Technologies - Developer Documentation
+
+Coming soon.
+
+## ![](./assets/images/legal.svg) Rechtliche Hinweise
+- [Datenschutzerklärung MYDATA](https://www.mydata-control.de/privacy_policy.html)
+- [Datenschutzerklärung GitHub](https://docs.github.com/de/site-policy/privacy-policies/github-privacy-statement)
+- [Impressum](https://www.iese.fraunhofer.de/de/impressum.html)
